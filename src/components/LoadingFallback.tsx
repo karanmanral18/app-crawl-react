59a4d4bdd54e7bb7d231bf59083ea24b61b75bf7
Karan Manral <karanmanral18@gmail.com>
2024-06-29 00:04:37 +0530

    add react loader

--- src/components/LoadingFallback.tsx
+++ src/components/LoadingFallback.tsx
@@ -1,9 +1,44 @@
-const LoadingFallback = () => {
-  return (
-    <div>
-      <h1>Loading...</h1>
-    </div>
-  );
-};
+import React from 'react'
+import ReactDom from 'react-dom'
+import { ThreeDots } from "react-loader-spinner";
+
+const OVERLAY_STYLES = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, .7)',
+  zIndex: 1000
+} as React.CSSProperties
+
+const LOADER_STYLES = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  padding: '50px',
+  zIndex: 1000
+} as React.CSSProperties
+
+const LoadingFallback: React.FC = () => {
+  return ReactDom.createPortal(
+    <>
+      <div style={OVERLAY_STYLES} />
+      <div style={LOADER_STYLES}>
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#4fa94d"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          visible={true}
+        />
+      </div>
+    </>,
+    document.getElementById('loader')!
+  )
+}
 
 export default LoadingFallback;
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
+import queryClient from './queries/queryClient.ts';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
